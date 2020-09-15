20188f328807d0f8cc2f9fe1cb52cfd8ec0a18be
Steven Barragán <me@steven.mx>
2020-09-14 17:22:25 -0700

    Refactor instructions and variables generation

--- src/main.rs
+++ src/main.rs
@@ -3,77 +3,202 @@ extern crate pest;
 extern crate pest_derive;
 
 use pest::Parser;
+use std::collections::HashMap;
 
 #[derive(Parser)]
 #[grammar = "universal.pest"]
 pub struct UniversalParser;
 
+mod instruction;
+
 fn main() {}
 
-trait Wast {
-    fn to_wast(&self) -> String;
-}
+type Name = String;
 
-struct VariableSetup {
-    variable: String,
+type Variables = HashMap<String, String>;
+
+struct Variable {
+    name: String,
     kind: String,
 }
 
-impl Wast for VariableSetup {
-    fn to_wast(&self) -> String {
-        format!("(local ${} {})", self.variable, self.kind)
-    }
+#[derive(PartialEq)]
+enum Language {
+    BasicOperation,
+    ConstantGet,
+    NativeType,
+    Number,
+    NumberGet,
+    VariableGet,
+    VariableName,
+    VariableSet,
 }
 
-struct VariableGet {
-    variable: String,
+struct Instruction<'a> {
+    kind: Language,
+    name: Name,
+    message: Option<&'a str>,
+    params: Vec<Instruction<'a>>,
+    valueType: String
 }
 
-impl Wast for VariableGet {
+impl<'a> Instruction<'a> {
     fn to_wast(&self) -> String {
-        format!("(local.get ${})", self.variable)
+        if self.kind == Language::VariableName {
+            return format!("${}", self.name)
+        }
+
+        let mut parts: Vec<String> = vec![];
+
+        if let Some(message) = self.message {
+            parts.push(format!("{}.{}", &self.name, message))
+        } else {
+            parts.push(format!("{}", &self.name))
+        }
+
+        let mut params: Vec<String> = vec!();
+
+        for param in &self.params {
+            parts.push(param.to_wast());
+        }
+
+        let mut result = parts.join(" ");
+
+        if parts.len() > 1 {
+            result = format!("({})", result);
+        }
+
+        result
     }
 }
 
 use pest::error::Error;
+use pest::iterators::Pair;
+
+fn build_instruction<'a>(pair: Pair<Rule>, variables: &Variables) -> Result<Instruction<'a>, Error<Rule>> {
+    match pair.as_rule() {
+        Rule::variable_setup => {
+            let mut inner = pair.into_inner();
+
+            let name = inner.next().unwrap().as_str();
+            let kind = inner.next().unwrap().as_str();
+
+            let instruction = Instruction {
+                kind: Language::VariableSet,
+                name: "local".to_string(),
+                message: None,
+                valueType: kind.to_owned(),
+                params: vec![
+                    Instruction {
+                        kind: Language::VariableName,
+                        name: name.to_string(),
+                        message: None,
+                        valueType: kind.to_owned(),
+                        params: vec![],
+                    },
+                    Instruction {
+                        kind: Language::NativeType,
+                        name: kind.to_string(),
+                        message: None,
+                        valueType: kind.to_owned(),
+                        params: vec![],
+                    },
+                ]
+            };
+
+            Ok(instruction)
+        },
+        Rule::variable_get => {
+            let mut inner = pair.into_inner();
+
+            let name = inner.next().unwrap().as_str();
+            let variable = variables.get(name);
+
+            if let Some(variable) = variable {
+                let instruction = Instruction {
+                    kind: Language::VariableGet,
+                    name: "local".to_string(),
+                    message: Some("get"),
+                    valueType: variable.to_string(),
+                    params: vec![
+                        Instruction{
+                            kind: Language::VariableName,
+                            name: name.to_string(),
+                            message: None,
+                            valueType: variable.to_string(),
+                            params: vec![]
+                        },
+                    ]
+                };
+
+                Ok(instruction)
+            } else {
+                panic!("No variable found")
+            }
+        },
+        Rule::number_get => {
+            let mut inner = pair.into_inner();
+            let value = inner.next().unwrap().as_str();
+
+            let instruction = Instruction {
+                kind: Language::NumberGet,
+                name: "i32".to_string(),
+                message: Some("const"),
+                valueType: "i32".to_string(),
+                params: vec![
+                    Instruction {
+                        kind: Language::Number,
+                        name: value.to_string(),
+                        message: None,
+                        valueType: "i32".to_string(),
+                        params: vec![]
+                    }
+                ]
+            };
+
+            Ok(instruction)
+        },
+        _ => panic!("WTF")
+    }
+}
+
+fn build_block<'a>(pair: Pair<Rule>) -> Result<Vec<Instruction<'a>>, Error<Rule>> {
+    let mut variables: Variables = HashMap::new();
+    let mut instructions: Vec<Instruction> = vec![];
+
+    for rule in pair.into_inner() {
+        let instruction = build_instruction(rule, &variables)?;
+
+        if instruction.kind == Language::VariableSet {
+            let name = instruction.params[0].name.to_string();
+            let value_type = instruction.params[1].name.to_string();
+
+            variables.insert(name, value_type);
+        }
+
+        instructions.push(instruction);
+    }
+
+    Ok(instructions)
+}
 
 pub fn parse_string(original: &str) -> Result<String, Error<Rule>> {
-    let pair = UniversalParser::parse(Rule::instruction, original)?.next().unwrap();
-    let mut instructions: Vec<Box<dyn Wast>> = Vec::new();
+    let pair = UniversalParser::parse(Rule::instructions, original)?.next().unwrap();
 
-    for instruction in pair.into_inner() {
-        match instruction.as_rule() {
-            Rule::variable_setup => {
-                let mut inner = instruction.into_inner();
-
-                let variable = inner.next().unwrap().as_str().to_string();
-                let kind = inner.next().unwrap().as_str().to_string();
-
-                instructions.push(Box::new(VariableSetup { variable , kind }));
-            },
-            Rule::variable_get => {
-                println!("{}", instruction);
-
-                let mut inner = instruction.into_inner();
-                let variable = inner.next().unwrap().as_str().to_string();
-
-                instructions.push(Box::new(VariableGet { variable }));
-            }
-            _ => ()
-        }
-    }
+    let instructions = build_block(pair)?;
 
     Ok(to_wasm(instructions))
 }
 
-fn to_wasm(instructions: Vec<Box<Wast>>) -> String {
-    let mut result = String::new();
+
+fn to_wasm(instructions: Vec<Instruction>) -> String {
+    let mut result: Vec<String> = vec![];
 
     for instruction in instructions {
-        result += &(*instruction).to_wast();
+        result.push(instruction.to_wast());
     }
 
-    result
+    result.join(" ")
 }
 
 #[cfg(test)]
@@ -84,6 +209,11 @@ mod test {
     fn variables() {
         assert_eq!(parse_string("let x: i32"), Ok("(local $x i32)".to_string()));
 
-        assert_eq!(parse_string("x"), Ok("(local.get $x)".to_string()));
+        assert_eq!(parse_string("let x: i32; x"), Ok("(local $x i32) (local.get $x)".to_string()));
+    }
+
+    #[test]
+    fn constants() {
+        assert_eq!(parse_string("42"), Ok("(i32.const 42)".to_string()));
     }
 }
