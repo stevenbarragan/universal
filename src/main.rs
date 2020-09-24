06abe6ddd4eafed974c7e5fc783b9bcd0753c5fd
Steven Barragán <me@steven.mx>
2020-09-24 15:01:41 -0700

    update #to_ast to return return functions as well

--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,19 @@ fn main() {}
 type Variables = HashMap<String, String>;
 
 #[derive(Debug, PartialEq, Clone, Hash, Eq)]
-pub enum Kind {
+pub enum ValueType {
     Integer,
+    Float,
     Other(String)
 }
 
 #[derive(Debug, PartialEq, Clone, Hash, Eq)]
 pub enum Language {
-    Variable(String, Kind),
+    Variable(String, ValueType),
     Number(String),
-    Infix(Operation, Box<Language>, Box<Language>)
+    Infix(Operation, Box<Language>, Box<Language>),
+    Function(String, Vec<(String, ValueType)>, Vec<ValueType>, Vec<Language>),
+    Kind(ValueType)
 }
 
 #[derive(Debug, PartialEq, Clone, Hash, Eq)]
@@ -48,10 +51,7 @@ fn build_ast(pair: Pair<Rule>, variables: &Variables) -> Result<Language, Error<
             let kind_str = variables.get(&name);
 
             if let Some(kind_name) = kind_str {
-                let kind: Kind = match kind_name.as_str() {
-                    "Int" => Kind::Integer,
-                    x => Kind::Other(x.to_string())
-                };
+                let kind: ValueType = str_to_value_type(kind_name);
 
                 Ok(Language::Variable(name, kind))
             } else {
@@ -65,8 +65,8 @@ fn build_ast(pair: Pair<Rule>, variables: &Variables) -> Result<Language, Error<
             let a = inner.next().unwrap().as_str();
 
             let kind = match a {
-                "Int" => Kind::Integer,
-                x => Kind::Other(x.to_string())
+                "Int" => ValueType::Integer,
+                x => ValueType::Other(x.to_string())
             };
 
             Ok(Language::Variable(name, kind))
@@ -87,7 +87,61 @@ fn build_ast(pair: Pair<Rule>, variables: &Variables) -> Result<Language, Error<
             };
 
             Ok(Language::Infix(operation, Box::new(left), Box::new(right)))
-        }
+        },
+        Rule::function_def => {
+            let mut inner = pair.into_inner();
+
+            let name = inner.next().unwrap().as_str().to_string();
+
+            let mut returns = vec![];
+            let mut params = vec![];
+            let mut instructions = vec![];
+
+            let mut elem = inner.next().unwrap();
+
+            println!("elem: {:?}", elem);
+
+            if elem.as_rule() == Rule::params {
+                println!("{:?}", elem);
+
+                let mut pair = elem.into_inner();
+
+                while let Some(rule) = pair.next() {
+                    let mut par = rule.into_inner();
+
+                    let name = pair.next().unwrap().as_str().to_string();
+                    let kind_str = par.next().unwrap().as_str();
+
+                    let kind = str_to_value_type(kind_str);
+
+                    params.push((name, kind));
+                }
+
+                elem = inner.next().unwrap();
+            }
+
+            if elem.as_rule() == Rule::returns {
+                let mut pair = elem.into_inner();
+
+                while let Some(rule) = pair.next() {
+                    let kind = str_to_value_type(rule.as_str());
+
+                    returns.push(kind);
+                }
+
+                elem = inner.next().unwrap();
+            }
+
+            if elem.as_rule() == Rule::block {
+                let mut pair = elem.into_inner();
+
+                while let Some(instruction) = pair.next() {
+                    instructions.push(build_ast(instruction, variables)?);
+                }
+            }
+
+            Ok(Language::Function(name, params, returns, instructions))
+        },
         _ => panic!("WTF")
     }
 }
@@ -114,11 +168,11 @@ mod test {
     fn variables() {
         let mut variables = HashMap::new();
 
-        assert_eq!(to_ast("x: Int", &mut variables), Ok(Variable("x".to_string(), Kind::Integer)));
+        assert_eq!(to_ast("x: Int", &mut variables), Ok(Variable("x".to_string(), ValueType::Integer)));
 
         variables.insert("x".to_string(), "Int".to_string());
 
-        assert_eq!(to_ast("x", &mut variables), Ok(Variable("x".to_string(), Kind::Integer)));
+        assert_eq!(to_ast("x", &mut variables), Ok(Variable("x".to_string(), ValueType::Integer)));
     }
 
     #[test]
@@ -131,6 +185,25 @@ mod test {
         variables.insert("x".to_string(), "Int".to_string());
 
         assert_eq!(to_ast("x + 1", &mut variables),
-            Ok(Infix(Operation::Add, Box::new(Variable ("x".to_string(), Kind::Integer)), Box::new(Number("1".to_string())))));
+            Ok(Infix(Operation::Add, Box::new(Variable ("x".to_string(), ValueType::Integer)), Box::new(Number("1".to_string())))));
+    }
+
+    #[test]
+    fn functions() {
+        let mut variables = HashMap::new();
+
+        let result = to_ast(
+        "fn hello(): Int
+            42
+        end", &mut variables);
+
+        let expected = Function(
+            "hello".to_string(),
+            vec![],
+            vec![ValueType::Integer],
+            vec![Number("42".to_string())]
+        );
+
+        assert_eq!(result, Ok(expected));
     }
 }
