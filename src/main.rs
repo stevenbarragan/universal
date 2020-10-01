ab4f2f30dddd39de1daddf60d7be89b6706e9e90
Steven Barragán <me@steven.mx>
2020-09-30 18:34:27 -0700

    Little refactor

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,6 @@ pub struct UniversalParser;
 
 extern crate wat;
 
-
 use wasmer_runtime::{
     instantiate,
     DynFunc,
@@ -31,16 +30,13 @@ struct InputValidator {}
 
 impl Validator for InputValidator {
     fn validate(&self, ctx: &mut ValidationContext) -> Result<ValidationResult, ReadlineError> {
-        use ValidationResult::{Incomplete, Invalid, Valid};
+        use ValidationResult::{Incomplete, Valid};
         let input = ctx.input();
 
-	let result = if let Ok(_) = UniversalParser::parse(Rule::language, input) {
-            Valid(None)
-        } else {
-            Incomplete
-        };
-
-        Ok(result)
+        match UniversalParser::parse(Rule::language, input) {
+            Ok(_) => Ok(Valid(None)),
+            _ => Ok(Incomplete)
+        }
     }
 }
 
@@ -71,16 +67,17 @@ fn main() -> anyhow::Result<()> {
 
                 let import_object = imports! {};
 
-                if let Ok(instance) = instantiate(&binary, &import_object) {
-                    let main = instance
-                        .exports
-                        .get::<DynFunc>("main")?;
+                match instantiate(&binary, &import_object) {
+                    Ok(instance) => {
+                        let main = instance.exports.get::<DynFunc>("main")?;
 
-                    match main.call(&[]) {
-                        Ok(result) => println!("=> {:?}", result),
-                        e => println!("{:?}", e)
-                    }
-                };
+                        match main.call(&[]) {
+                            Ok(result) => println!("=> {:?}", result),
+                            e => println!("{:?}", e)
+                        }
+                    },
+                    Err(e) => println!("Err: {:?}", e)
+                }
 	    },
             Err(ReadlineError::Interrupted) => {
 		println!("CTRL-C");
